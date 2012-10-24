942b1c3c52c33faa5d86f8534c144f7d8d66ad5a
Makis Tracend <makis.tracend@gmail.com>
2012-10-23 21:57:35 -0700

    Adding few more files

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# install things...
+
+# Assume everyone's on OS X and run Homebrew (*nix-ers beware)
+. setup/homebrew
+
+# setup other software
+. setup/node
+. setup/bash
+
+cd "$(dirname "$0")"
+git pull
+
+function doIt() {
+	rsync --exclude ".git/" --exclude "script/" --exclude ".DS_Store" --exclude "install.sh" --exclude "readme.md" -av . ~
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
+fi
+unset doIt
+source ~/.bash_profile
